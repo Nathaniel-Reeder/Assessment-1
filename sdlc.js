b0fee0c9971efe70facc644863d135c787dbbd72
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-02-16 16:40:44 -0700

    Finished sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -20,3 +20,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements ="Performed with input from the client and other stakeholders, determines what will need to be done to produce the software. Often done at the same time as planning."
+
+const design = "This stage focuses on designing the product. It involves product architects and developers who will present a design of the product. They may present more than one design approach."
+
+const Implementation = "This is the stage where coding takes place and software is created."
+
+const testingAndIntegration = "During this stage, pieces of code are tested, put together, and tested to ensure bugs aren't introduced. Software may need further work after this step to be ready to release."
+
+const maintain = "This step is ongoing, and involves continued maintenance of the code during its lifetime. This includes fixing new bugs and potentially providing updates that improve functionality."
--- variables.js
+++ variables.js
@@ -1,17 +1,17 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+let myName = "Nate"
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+let favNum = 8
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 /*
